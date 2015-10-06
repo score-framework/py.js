3d4b27a577717c2b59af6c58da75d9044e862eea
Necdet Can Atesman <c.atesman@gmx.net>
2015-10-06 15:09:55 +0200

    Added license and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,21 @@ setup(
     url='http://score-framework.org',
     keywords='score framework web javascript',
     packages=['score.js'],
+    license='LGPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General '
+            'Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
     install_requires=[
         'score.webassets >= 0.1',
     ],
