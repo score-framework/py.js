62a6efcbd76906b6390c0fc59285a6d14eee6412
Necdet Can Ateşman <can@strg.at>
2017-05-15 14:27:28 +0200

    Release: 0.4.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.js',
-    version='0.3.1',
+    version='0.4.0',
     description='Helpers for managing javascript with The SCORE Framework',
     long_description=README,
     author='strg.at',
